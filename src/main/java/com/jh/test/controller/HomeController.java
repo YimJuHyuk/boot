cdef61faca0b2b253e6927f6aada245ca8006443
JuHyuk <wngur94@naver.com>
2021-05-29 01:19:15 +0900

    롬복 및 로그 추가

--- src/main/java/com/jh/test/controller/HomeController.java
+++ src/main/java/com/jh/test/controller/HomeController.java
@@ -6,11 +6,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class HomeController {
 
 	@RequestMapping(value = "/home", method=RequestMethod.GET)
 	public String goHome(HttpServletRequest request) {
+		
+		log.error("호출한다아");
+		
 		return "content/home";
 	}
 	
