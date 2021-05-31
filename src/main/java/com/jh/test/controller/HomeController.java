0de7c3b05321d8f708f5a83aaa5245a119e89b13
YimJuHyuk <wngur94@naver.com>
2021-05-31 14:47:49 +0900

    123

--- src/main/java/com/jh/test/controller/HomeController.java
+++ src/main/java/com/jh/test/controller/HomeController.java
@@ -5,14 +5,17 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
 	@RequestMapping(value = "/", method=RequestMethod.GET)
-	public String goHome(HttpServletRequest request) {
+	public ModelAndView goHome(HttpServletRequest request) {
 		System.out.println("page move~~~~~");
-		return "content/home";
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("content/home");
+		return mv;
 	}
 	
 }
